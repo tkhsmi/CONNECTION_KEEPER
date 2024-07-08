2e1e72039cce33af5cbf83415b939377a5f22941
tkhsmi <mai.teraoka2525@gmail.com>
2024-07-08 09:39:50 +0900

    Add: devise導入

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,16 @@
 
 Rails.application.routes.draw do
   root 'top#index'
+
+  devise_for :admins, path: 'admin7bi170', controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
 end
