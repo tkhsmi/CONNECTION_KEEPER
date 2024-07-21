67d5e14956c481249debc8ce505e9c62010caba3
tkhsmi <takahashi.mai2255@gmail.com>
2024-07-21 21:51:38 +0900

    Update: Rubocop #15

--- spec/requests/users/dashboard_spec.rb
+++ spec/requests/users/dashboard_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Users::Dashboards', type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/users/dashboard/index"
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/users/dashboard/index'
       expect(response).to have_http_status(:success)
     end
   end
