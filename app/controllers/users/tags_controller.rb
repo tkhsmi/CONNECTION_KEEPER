9f8a8f1684ad0484dfd3c2b1b6ed4663051f5aba
tkhsmi <takahashi.mai2255@gmail.com>
2024-07-24 08:02:11 +0900

    Update: タグ追加処置実装

--- app/controllers/users/tags_controller.rb
+++ app/controllers/users/tags_controller.rb
@@ -1,15 +1,37 @@
 class Users::TagsController < ApplicationController
-  def index; end
+  before_action :set_tag, only: %i[edit update]
 
-  def show; end
+  def index
+    @tags = Tag.all
+  end
 
-  def new; end
+  def new
+    @tag = Tag.new
+  end
 
-  def create; end
+  def create
+    @tag = Tag.new(tag_params)
+    @tag.user_id = current_user.id
+    if @tag.save
+      redirect_to users_tags_path
+    else
+      render :new
+    end
+  end
 
   def edit; end
 
   def update; end
 
   def destroy; end
+
+  private
+
+  def set_tag
+    @tag = Tag.find(params[:id])
+  end
+
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
 end
