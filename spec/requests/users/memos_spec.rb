4e0683aeb83c9b33cc7a13fc24b3e9339c55587d
tkhsmi <takahashi.mai2255@gmail.com>
2024-08-15 21:53:38 +0900

    Update: RSpecが通るように修正

--- spec/requests/users/memos_spec.rb
+++ spec/requests/users/memos_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Users::Memos', type: :request do
   describe 'GET /index' do
-    xit 'returns http success' do
+    it 'returns http success' do
+      skip '後程ログインできるようにする'
       get '/users/memos'
       expect(response).to have_http_status(:success)
     end
--- spec/requests/users/people_spec.rb
+++ spec/requests/users/people_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Users::People', type: :request do
   describe 'GET /index' do
-    xit 'returns http success' do
+    it 'returns http success' do
+      skip '後程ログインできるようにする'
       get '/users/people'
       expect(response).to have_http_status(:success)
     end
