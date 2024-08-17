d613a868291e46dd3169c52f43a971f475791c2e
tkhsmi <takahashi.mai2255@gmail.com>
2024-08-17 16:17:48 +0900

    Update: 前のプロジェクト名が残っていた箇所をCONNECTION KEEPERに置換

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "relationships_memo_v1_production"
+  # config.active_job.queue_name_prefix = "conection_keeper_production"
 
   config.action_mailer.perform_caching = false
 
