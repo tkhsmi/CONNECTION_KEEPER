195b4b804400d9e51c1501a8a6adf039361dcb19
tkhsmi <takahashi.mai2255@gmail.com>
2024-08-21 11:45:50 +0900

    Update: 不要な空白行削除

--- app/controllers/users/people_controller.rb
+++ app/controllers/users/people_controller.rb
@@ -32,9 +32,7 @@ class Users::PeopleController < ApplicationController
 
   def destroy
     person = Person.find(params[:id])
-
     person.destroy if person.user_id == current_user.id
-
     redirect_to users_people_path
   end
 
